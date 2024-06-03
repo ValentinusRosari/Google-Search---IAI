b930a75a6278ddb136ecb3ed1f00ba1af6ac57d8
zildirayalfirli <zildirayalfirli@gmail.com>
2024-06-04 01:07:03 +0700

    fix index.js 2

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const port = 5000;
 
-app.get("/", (req, res) => res.send("On"));
-app.get("/search", require("./api/search"));
+app.get("/search/:medicineName", (req, res) => {
+  const medicineName = req.params.medicineName;
+  const googleSearchUrl = `https://www.google.com/search?q=Beli+obat+${encodeURIComponent(
+    medicineName
+  )}`;
+  res.json({ url: googleSearchUrl });
+});
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
+});
